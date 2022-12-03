c5894f4f72dac89a3405291a84b6a9cd3a80a0b7
sagitace <aaronlabini27.official@gmail.com>
2022-12-03 11:57:43 +0800

    update

--- resources/views/home/offer.blade.php
+++ resources/views/home/offer.blade.php
@@ -4,11 +4,14 @@
       <div class="container ">
         <div class="row">
           <div class="col-md-6  ">
-            <div class="box ">
+            <div class="box" style="position:relative;">
               <div class="img-box">
                 <img src="home/images/white-almond-latte.png" alt="">
               </div>
-              <div class="detail-box">
+              <div class="detail-box" >
+
+
+
                 <h5>
                   Christmas Sale!
                 </h5>
@@ -16,11 +19,13 @@
                   Up to <span>20%</span> Off
                 </h6>
 
+
               </div>
+              <img src="{{asset('home/images/c2.png')}}" class="c2">
             </div>
           </div>
           <div class="col-md-6  ">
-            <div class="box ">
+            <div class="box " style="position: relative;">
               <div class="img-box">
                 <img src="home/images/cheesy-nachos.png" alt="" style="margin-top: -30px;">
               </div>
@@ -33,6 +38,7 @@
                 </h6>
 
               </div>
+              <img src="{{asset('home/images/c2.png')}}" class="c2">
             </div>
           </div>
         </div>
